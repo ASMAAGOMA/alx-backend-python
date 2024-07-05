91ab6919adc8fdcdfd0bcb5744dc8260875824dc
asmaagoma <asmaagadallaah@gmail.com>
2024-07-06 00:08:18 +0300

    :"(

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -4,22 +4,21 @@ sos
 """
 
 
-from typing import Tuple, List
+from typing import List, Tuple, Any
 
 
-def zoom_array(lst: Tuple, factor: int = 2) -> List:
+def zoom_array(lst: Tuple[Any, ...], factor: int = 2) -> List[Any]:
     """
     sos
     """
-    zoomed_in: List = [
+    zoomed_in: List[Any] = [
         item for item in lst
         for i in range(factor)
     ]
     return zoomed_in
 
 
-array = [12, 72, 91]
+array: Tuple[int, ...] = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
-
 zoom_3x = zoom_array(array, 3)
