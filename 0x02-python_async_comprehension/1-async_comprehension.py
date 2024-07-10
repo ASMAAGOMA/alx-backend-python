e247fc582d89d41639dfd56ef644d03c1c58c13e
asmaagoma <asmaagadallaah@gmail.com>
2024-07-10 08:32:04 +0300

    :"(

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+async loop
+"""
+
+
+from typing import Generator
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> Generator[float, None, None]:
+    result = [i async for i in async_generator()]
+    return result
